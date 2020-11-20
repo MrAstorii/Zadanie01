90b6947934c2c79c7d469e17813792df1d5d73c1
MrAstorii <72743039+MrAstorii@users.noreply.github.com>
2020-11-20 01:10:41 +0100

    Create Granat.gd

---
+++ Zadanie01/Granat.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+var _gravity = 0
+
+var _movement = Vector2()
+
+func shoot(directional_force, gravity):
+	_movement = directional_force
+	_gravity = gravity
+	set_physics_process(true)
+
+func _physics_process(delta):
+	_movement.y += delta * _gravity
+	
+	move_and_collide(_movement)
+	
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
