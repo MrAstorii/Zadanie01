3e8432c89e8d0145010ed829fcee7b88dd6f354e
MrAstorii <72743039+MrAstorii@users.noreply.github.com>
2020-11-20 01:07:25 +0100

    Update FF.gd

--- Zadanie01/FF.gd
+++ Zadanie01/FF.gd
@@ -1,10 +1,16 @@
 extends KinematicBody2D
 
+
 const UP = Vector2(0, -1)
 const GRAVITY = 20
 const SPEED = 300
 const JUMP_HEIGHT = -600
 var motion = Vector2()
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
 
 func _physics_process(delta):
 	motion.y += GRAVITY
@@ -27,4 +33,11 @@ func _physics_process(delta):
 			
 	
 	motion = move_and_slide(motion, UP)
+	
 	pass
+
+func _process(delta):
+	position += motion * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
