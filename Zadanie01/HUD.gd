07b78e4ed98907240b3feb995644fd40db8b7bb0
MrAstorii <72743039+MrAstorii@users.noreply.github.com>
2020-11-25 18:25:20 +0100

    Update HUD.gd

--- Zadanie01/HUD.gd
+++ Zadanie01/HUD.gd
@@ -5,11 +5,18 @@ signal start_game
 func _ready():
 	$InfoLabel.hide()
 	$EndLabel.hide()
+	$Skip.hide()
+
+func show_message_victory():
+	$VictoryLabel.show()
+	$MessageTimer.start()
+	$Panel.show()
 
 func show_message():
 	$InfoLabel.show()
 	$MessageTimer.start()
 	$Panel.show()
+	$Skip.show()
 
 func show_message_end():
 	$EndLabel.show()
@@ -22,7 +29,7 @@ func show_game_over():
 	$StartButton.show()
 	$MessageLabel.text = "Przy moście w Sarajewie"
 	$MessageLabel.show()
-	$Panel.show()
+	$Most.show()
 
 func update_score(score):
 	$ScoreLabel.text = str(score)
@@ -31,11 +38,20 @@ func _on_MessageTimer_timeout():
 	$Panel.hide()
 	$InfoLabel.hide()
 	$EndLabel.hide()
+	$Skip.hide()
 
 
 func _on_StartButton_pressed():
 	$StartButton.hide()
 	$MessageLabel.hide()
+	$Most.hide()
 	emit_signal("start_game")
 	
+
+
+
+func _on_Skip_pressed():
+	$Skip.hide()
+	$InfoLabel.hide()
+	$Panel.hide()
 
