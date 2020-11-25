c265244cfe871f683c7eb8d96d55fc31bcbd1731
MrAstorii <72743039+MrAstorii@users.noreply.github.com>
2020-11-25 18:25:34 +0100

    Update Świat.gd

--- Zadanie01/Świat.gd
+++ Zadanie01/Świat.gd
@@ -6,13 +6,18 @@ func game_over():
 	$ScoreTimer.stop()
 	$HUD.show_game_over()
 	get_tree().call_group("Pociski", "queue_free")
+	$Muzyka.stop()
+	$Open.play()
 
 func new_game():
+	$Open.stop()
 	score = 0
 	$FF.start($StartPosition.position)
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message()
+	$Muzyka.play()
+	
 
 func _on_StartTimer_timeout():
 	$ScoreTimer.start()
